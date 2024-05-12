8994072af06cd38d72732c3bfc462fef98bbae40
Martin Monperrus <martin.monperrus@gnieh.org>
2024-05-12 12:53:47 +0200

    up

--- src/manual_analysis_martin/manual_patch_analysis_martin.py
+++ src/manual_analysis_martin/manual_patch_analysis_martin.py
@@ -4,6 +4,9 @@ import tqdm
 import fire
 import os
 import re
+import hashlib
+import glob
+import subprocess
 
 from typing import Optional, List
 from pygments import highlight
@@ -22,15 +25,23 @@ def compute_diff(
         if context_len is not None
         else max(len(buggy_code), len(fixed_code))
     )
-    return "".join(
-        list(
-            difflib.unified_diff(
-                buggy_code.splitlines(keepends=True),
-                fixed_code.splitlines(keepends=True),
-                n=context_len,
-            )
-        )
-    )
+    with open("/tmp/buggy.java","w") as f: f.write(buggy_code+"\n")
+    with open("/tmp/fixed_code.java","w") as f: f.write(fixed_code+"\n")
+    # we want to ignore whitespace changes with -w which does not exist in difflib.unified_diff
+    # with git diff, we even the name of the changed function in the diff, which helps a lot
+    cmd = ["git","diff","--patience","-U10", "-w","/tmp/buggy.java","/tmp/fixed_code.java"]
+    # print(" ".join(cmd))
+    # print(dir(subprocess.run(cmd)))
+    return subprocess.run(cmd, capture_output=True).stdout
+    # return "".join(
+    #     list(
+    #         difflib.unified_diff(
+    #             buggy_code.splitlines(keepends=True),
+    #             fixed_code.splitlines(keepends=True),
+    #             n=context_len,
+    #         )
+    #     )
+    # )
 
 
 def remove_java_comments(source: str) -> str:
@@ -109,7 +120,7 @@ def remove_empty_lines(source):
     return re.sub(r"^\s*$\n", "", source, flags=re.MULTILINE)
 
 
-def manual_analysis(input_file: str, output_file: str):
+def manual_analysis(input_file: str):
     """
     Perform manual analysis on the bugs in the input file and write the results to the output file.
 
@@ -118,6 +129,9 @@ def manual_analysis(input_file: str, output_file: str):
     - output_file: str: The path to the output file to write the results.
     """
     # Load fixed functions
+    
+    output_file = "martin/"+os.path.basename(input_file.replace(".jsonl", "_martin.jsonl"))
+    
     fixed_functions = {}
     with open("defects4j-fixed_code.jsonl") as f:
         # one bug per line
@@ -136,10 +150,16 @@ def manual_analysis(input_file: str, output_file: str):
         for line in f.readlines():
             bug = json.loads(line)
             bugs.append(bug)
-
+    total_to_analyze = 0
     # Perform manual analysis
     for bug in tqdm.tqdm(bugs, "Number of bugs processed: "):
         try:
+            if "bug_id" not in bug:
+                with open("problems.log","a") as f: f.write("a bug has no bug_id in "+input_file+"\n")
+                continue
+            if "test_results" not in bug:
+                with open("problems.log","a") as f: f.write(bug["bug_id"]+" has no test_results\n")
+                continue
             # Skip if the bug does not have any plausible patch
             if "Disagree" not in bug["test_results"]:
                 continue
@@ -154,9 +174,20 @@ def manual_analysis(input_file: str, output_file: str):
                 continue
 
             for i, patch in enumerate(bug["patches"]):
+                print(bug["bug_id"], bug["test_results"][i])
+
                 # Skip if the patch is not plausible
                 if bug["test_results"][i] != "Disagree":
+                    
                     continue
+                
+                total_to_analyze += 1
+                # continue
+
+                cachename = "martin/"+hashlib.sha256(patch.encode()).hexdigest()
+                if os.path.exists(cachename):
+                    continue
+                
 
                 # Print ground truth and plausible patch side-by-side in the terminal
                 ground_truth = compute_diff(
@@ -187,9 +218,13 @@ def manual_analysis(input_file: str, output_file: str):
                 print("0 -> different, 1 -> equivalent")
                 result = int(input("Option: "))
                 # If we find a semantic match we break the loop
+                with open(cachename,"w") as f:
+                    f.write(json.dumps({"bug_id":bug["bug_id"],"patch":patch,"result":result}))
                 if result == 1:
                     bug["test_results"][i] = "Semantical match"
                     break
+
+                
 
                 # Clear the terminal
                 os.system("cls" if os.name == "nt" else "clear")
@@ -197,7 +232,11 @@ def manual_analysis(input_file: str, output_file: str):
             # Write the bug to the output file
             with open(output_file, "a+") as f:
                 f.write(json.dumps(bug) + "\n")
+    # python manual_patch_analysis_martin.py  | grep total_to_analyze | awk '{ total += $2 } END { print total }' 
+    print("total_to_analyze",total_to_analyze)
 
 
 if __name__ == "__main__":
-    fire.Fire(manual_analysis)
+    for i in glob.glob("defects4j/*")+glob.glob("humanevaljava/*"):
+        manual_analysis(i)
+    # fire.Fire(manual_analysis)
